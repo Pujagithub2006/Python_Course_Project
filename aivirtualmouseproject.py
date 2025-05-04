6ee06202b9317d4d370f0a043a0fbd6212a80d9d
Sanket Palwe <sanketpalwe2021@gmail.com>
2025-05-04 20:05:51 +0530

    Python Project Stage 1

---
+++ aivirtualmouseproject.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+import HandTrackingModule as htm
+import time
+import os
+import pyautogui
+
+#############################
+wCam, hCam = 640, 480
+frameR = 100  # Frame reduction
+smoothening = 7
+#############################
+
+pTime = 0
+plocX, plocY = 0, 0
+clocX, clocY = 0, 0
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+detector = htm.handDetector(maxHands=1)
+wScr, hScr = pyautogui.size()  # Get screen size
+
+while True:
+    # 1. Find hand landmarks
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+
+    # 2. Get the tip of index and middle fingers
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        # 3. Check which fingers are up
+        fingers = detector.fingersUp()
+        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
+
+        # 4. Moving mode: Only index finger is up
+        if fingers[1] == 1 and fingers[2] == 0:
+            # 5. Convert Coordinates
+            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+
+            # 6. Smoothen Values
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+
+            # 7. Move Mouse
+            pyautogui.moveTo(clocX, clocY)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+
+        # 8. Left Click: Index + middle finger up and close together
+        if fingers[1] == 1 and fingers[2] == 1:
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                pyautogui.click()
+
+        # 9. Right Click: Thumb + pinky close together
+        if fingers[0] == 1 and fingers[4] == 1:
+            length, img, lineInfo = detector.findDistance(4, 20, img)
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                pyautogui.click(button='right')
+
+        # 10. Open Drive A: Index + ring finger close
+        if fingers[1] == 1 and fingers[3] == 1:
+            length, img, lineInfo = detector.findDistance(8, 16, img)
+            if length < 40:
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                os.startfile(os.path.expanduser("~/Downloads"))
+
+    # 11. Frame rate
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    # 12. Display
+    cv2.imshow("AI Virtual Mouse", img)
+    if cv2.waitKey(1) == 27:  # ESC to quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
